cedb1d0aac6778f0b1ccfd3023800e0055f0053c
Mizuki <mizukiterai@gmail.com>
2024-03-05 17:06:56 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './pizza.png';
 import './App.css';
 
 function App() {
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          My favorite food and below is my LinkedIn Page
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/mizuki-terai-a35bb1166/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Linkedin Page
         </a>
       </header>
     </div>
